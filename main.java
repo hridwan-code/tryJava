99730296bbea0ad8c1e4e18c4aa2cffcac04eeba
HRidwan <hridwan0077@gmail.com>
2026-02-08 22:42:46 +0700

    basic java

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("kalau muncul berarti udah");
+        System.out.println("saya seorang mahasiswa di universitas pasundan dengan nama dan umur");
+
+        int umur = 19;
+        String nama = "ridwan";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
     }
 }
