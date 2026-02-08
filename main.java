9eefb104d7f74a74a7074ec62b578b19419f1b11
HRidwan <hridwan0077@gmail.com>
2026-02-08 22:41:50 +0700

    basic java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("kalau muncul berarti udah berhasil");
+        System.out.println("kalau muncul berarti udah");
     }
 }
